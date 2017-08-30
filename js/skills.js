73cd815e76fc45507b5084682d0d049e761dca11
JavierTrc <ja.troconis10@uniandes.edu.co>
2017-08-30 02:21:24 -0500

    Unified all content in a single page, added spyscroll navigation and jquery detects certain point in page

--- js/skills.js
+++ js/skills.js
@@ -1,5 +1,13 @@
-$(".skillbar").each(function(){
-  $(this).find(".skillbar-bar").animate({
-    width:$(this).attr("data-percent")
-  },4000);
+$(window).scroll(function() {
+  var hT = $("#skills").offset().top,
+    hH = $("#skills").outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+  if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+    $(".skillbar").each(function(){
+      $(this).find(".skillbar-bar").animate({
+        width:$(this).attr("data-percent")
+      },4000);
+    });
+  }
 });
