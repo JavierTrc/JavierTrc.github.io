d1c633d2a6ebd0488b72f893fb4ad06853a5a1db
JavierTrc <ja.troconis10@uniandes.edu.co>
2017-08-30 05:20:52 -0500

    Finished adding final details

--- js/skills.js
+++ js/skills.js
@@ -11,3 +11,32 @@ $(window).scroll(function() {
     });
   }
 });
+
+$(".card-img-overlay").hover(function(){
+  $(this).animate({
+    opacity:1
+  },500);
+}, function(){
+  $(this).animate({
+    opacity:0
+  },500);
+});
+
+$(".coming-soon").hover(function(){
+  $(this).css({border: "0 solid #428bca"}).animate({
+    borderWidth: 3
+  }, 250);
+},function () {
+  $(this).animate({
+    borderWidth: 0
+  }, 250);
+});
+
+$(".coming-soon").click(function(){
+  $(this).html("<h4 class='card-title'>???</h4><h6 class='card-subtitle mb-2 text-muted'>Coming Soon</h6><p class='card-text'>Voice recognition assistant (still defining)</p>");
+});
+
+
+
+
+                
